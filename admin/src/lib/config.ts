a9c0e5da962fff6a238f38cf6e86185907b4864c
Aswin Jothish <aswinjothish@gmail.com>
2026-02-11 10:56:24 +0530

    base url commit

--- admin/src/lib/config.ts
+++ admin/src/lib/config.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "https://qdp3nmw3-4000.inc1.devtunnels.ms//api/admin"
-// export const baseUrl = "http://localhost:4000/api/admin";
+//export const baseUrl = "https://qdp3nmw3-4000.inc1.devtunnels.ms//api/admin"
+export const baseUrl = "http://localhost:4000/api/admin";
 //export const baseUrl = "http://65.0.108.35:4000/api/admin";
 // export const baseUrl = "http://43.204.110.10:4000/api/admin";
 //export const baseUrl = "https://sunstar.wenoxo.in/api/admin";
