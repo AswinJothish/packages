03b951011b84a6942e46b7181003ecf86eb43e4e
Aswin Jothish <aswinjothish@gmail.com>
2026-02-11 11:21:11 +0530

    new commit baseurl imgbaseurl

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,5 +1,5 @@
 //export const baseUrl = "https://qdp3nmw3-4000.inc1.devtunnels.ms/api/user"
 export const baseUrl = "http://localhost:4000/api/user";
 //export const baseUrl = "http://43.204.110.10:4000/api/user";
-//export const ImgbaseUrl = "http://localhost:4000/api/admin/files/view?key=";
+export const ImgbaseUrl = "http://localhost:4000/api/admin/files/view?key=";
 //export const ImgbaseUrl = "http://43.204.110.10:4000/api/admin/files/view?key=";
